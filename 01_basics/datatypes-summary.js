ceb60bd0ea95abd128d1ba54472b9d781144c477
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-21 13:02:24 +0000

    DataType Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+//Video 9 (https://www.youtube.com/watch?v=suMvZWjjKbo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=9)
+/*types of datatypes
+1) Primitive
+2) Non-Primitive
+
+# Primitive Datatype:
+7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+// Reference (Non Primitive)
+Array, Objects, Functions
+*/
