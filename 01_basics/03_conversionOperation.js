524ef874b3a52cc0e49088e4f98417d24131ff4d
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-21 11:42:00 +0000

    conversion completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 // when we convert number for ex "33" to a number it will get easily converted to a number
 // when we convert a aphanumeric number to a number it will convert but the type will be NaN for ex "33abc" => NaN
@@ -13,14 +13,14 @@ console.log(valueInNumber)
 let bool = true
 
 let valueInNumber1 = Number(bool)
-console.log(typeof valueInNumber1)
-console.log(valueInNumber1)
+//console.log(typeof valueInNumber1)
+//console.log(valueInNumber1)
 
 //Convert 1 or 0 to a boolean
 let isLoggedIn = "abc"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -28,5 +28,42 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+/**************** Operations *************/
+let value = 3
+let negValue = -value
+
+//console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/3)
+//console.log(2%3)
+
+let str1 = "Hello"
+let str2 = " Abhinav"
+// console.log(str1 + str2)
+
+// NOT TO PERFORM SUCH CONVERSION 
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+1)
+// console.log(1+2+"1")
+// console.log(3 + 4 * 5 % 3)
+//console.log(+true)
+//console.log(+"")
+
+//Postfix increment
+let x = 3
+const y = x++
+console.log(`x:${x}, y:${y}`);
+console.log(`x:${x}, y:${y}`);
+
+//prefix increment
+let a = 3
+const b = ++a
+console.log(`a:${a}, b:${b}`)
