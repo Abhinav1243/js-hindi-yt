47471a3d08bdd692ce0221dc0fa59d117d97e1d4
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-22 11:48:00 +0000

    String completed for video 11

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "abhinav"
+const repoCount = 50
+
+//console.log(name + repoCount)
+
+// Recommemded Way or latest way to print a string
+console.log(`My name is ${name.toUpperCase()}, and my Repo Count ${repoCount}`)
+
+//Another way to declare String
+
+const gameName = new String('abhinavas')
+// console.log(gameName[0])
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase())
+// console.log(gameName.length)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('a'))
