9a01f128319ef7fe91abcc29c59a66a2d6f6fd85
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-22 10:28:38 +0000

    memory with datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -9,3 +9,35 @@
 // Reference (Non Primitive)
 Array, Objects, Functions
 */
+
+const heros = ["captain","spider","hulk"] //Array
+let myObj = {
+    name: "abhinav",
+    age: 18
+}   //Objects
+
+const myFunc = function(){
+    console.log("Hello World");
+}
+
+// console.log(typeof(myFunc))
+// console.log(typeof(myObj))
+// console.log(typeof(heros))
+// console.log(myFunc)
+
+let myYoutubename = "abc"
+let anothername = myYoutubename
+anothername = 'xyz'
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    name: "abhinav",
+    email: "abhinav@google.com"
+}
+
+let userTwo = userOne
+userTwo.email = 'abhinav@gmail.com'
+
+console.log(userOne.email)
+console.log(userTwo.email)
