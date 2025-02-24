3b2e4e644b708bb69c5dd42d3fffb05496deb644
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-24 08:23:26 +0000

    numbs and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+const score = 400
+console.log(score)
+
+//another way to declare a number
+const balance = new Number(100.564)
+console.log(balance)
+
+//Example for methods
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 1000000
+
+console.log(hundreds.toLocaleString('en-in'))
+
+// +++++++++++++++ Maths ++++++++++++++
+
