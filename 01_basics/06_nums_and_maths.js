b364bf0639e8fac6afda4c28c7ab7bc1866afef0
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-24 08:49:25 +0000

    Math and num complete

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -18,4 +18,16 @@ const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-in'))
 
 // +++++++++++++++ Maths ++++++++++++++
+console.log(Math)
+console.log(Math.abs(-4)) //convert negative to positive using absolute function
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.734))
+// console.log(Math.floor(4.6))
 
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
