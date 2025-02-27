bda1ec100e3282f9ab7a6014990ad4f04e90cc42
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-27 10:58:40 +0000

    Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,1 +1,8 @@
-// singleton
+// there are two ways to declare an object one is with literal and another one is using constructor
+// singleton - when you create an object using a constructor then singleton is the object created which means this is one of a kind
+
+// Object Literals
+
+const jSuser = {
+    name: "Abhinav"
+}
