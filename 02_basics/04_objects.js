6e905a06174aed66dc38254d3246cf6867aead6b
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-02-28 08:36:21 +0000

    object part 2 half

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,30 @@
+const tinderObject = new Object()
+
+// const tinderObject = {}
+
+tinderObject.id = "123abc"
+tinderObject.name = "sammy"
+tinderObject.isLoggedIn = false
+
+// console.log(tinderObject)
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname : {
+        userfullname: {
+            firstname: "abc",
+            lastname: "xyz"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign({}, obj1, obj2) // Object.assign(target, source)
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
