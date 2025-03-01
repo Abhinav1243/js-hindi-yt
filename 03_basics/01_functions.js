2368e948b0c36abab7bfd2bb6f383e734aae8a8e
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-03-01 12:58:21 +0000

    functions started

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+
+
+//Definition
+function sayMyName(){
+    console.log("a")
+    console.log("b")
+    console.log("c")
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    return number1 + number2
+}
+
+console.log(addTwoNumbers(3, 5))
