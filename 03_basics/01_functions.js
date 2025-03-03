83b6d935b63a7ec1ac0f8f1f85d31cafbf77aef9
Abhinav1243 <31155109+Abhinav1243@users.noreply.github.com>
2025-03-03 10:21:49 +0000

    function basics completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,4 +13,44 @@ function addTwoNumbers(number1, number2){
     return number1 + number2
 }
 
-console.log(addTwoNumbers(3, 5))
+// console.log(addTwoNumbers(3, 5))
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Unknown User")
+    }
+    else{
+        console.log(`${username} just logged in`)
+    }
+}
+
+// console.log(loginUserMessage("Abhinav"))
+loginUserMessage()
+
+
+// How to pass objects in function
+const user = {
+    username: "abhinav",
+    price: 99 
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "Sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray))
+
+console.log(returnSecondValue([200, 400, 100, 1000]))
